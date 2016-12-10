334399eeed01e3f2db1dd29d8f7911f409304c4f
filip <rakocz88@gmail.com>
2016-12-10 15:34:28 +0100

    cucumber_test_2

--- sdc-mail/src/test/java/com/pilaf/sdc/mail/test/integration/steps/MailSteps.java
+++ sdc-mail/src/test/java/com/pilaf/sdc/mail/test/integration/steps/MailSteps.java
@@ -2,12 +2,14 @@ package com.pilaf.sdc.mail.test.integration.steps;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.boot.test.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 
 import com.pilaf.sdc.mail.json.MailMsg;
+import com.pilaf.sdc.mail.model.MessageType;
 import com.pilaf.sdc.mail.model.OutputMsgDO;
 import com.pilaf.sdc.mail.test.integration.SendMailIntegrationTestBase;
 
@@ -17,15 +19,19 @@ import cucumber.api.java.en.When;
 
 public class MailSteps extends SendMailIntegrationTestBase {
 
-	private static final String RECIPENT_ADDRESS = "test@t.pl";
-	private static final String MSG_TEXT = "test1";
-
 	private ResponseEntity<OutputMsgDO> entity;
 	private List<OutputMsgDO> returnedList;
 	private OutputMsgDO sendMessage;
 
 	@Given("^I am a user with id (\\d+)$")
 	public void i_am_a_user_with_id(int arg1) throws Throwable {
+	}
+
+	@Given("^User (\\d+) send an email with text \"(.*?)\" and recipent \"(.*?)\"$")
+	public void user_send_an_email_with_text_and_recipent(long senderId, String messageText, String recipent)
+			throws Throwable {
+		outputMsgRepository
+				.save(new OutputMsgDO(0l, recipent, senderId, LocalDate.now(), messageText, MessageType.MAIL));
 	}
 
 	@When("^As user (\\d+) I send an email message with \"(.*?)\" to recipent \"(.*?)\"$")
@@ -49,12 +55,15 @@ public class MailSteps extends SendMailIntegrationTestBase {
 	public void i_should_have_message_in_filter_result(int amountOfMessages) throws Throwable {
 		assertThat("List should have " + amountOfMessages + " messages but had " + returnedList.size() + " messages",
 				returnedList.size() == amountOfMessages);
-		sendMessage = returnedList.get(0);
+		if (returnedList.size() > 0) {
+			sendMessage = returnedList.get(0);
+		}
 	}
 
 	@Then("^I should get a  messageoutput with message text \"(.*?)\" and recipent \"(.*?)\"$")
-	public void i_should_get_a_messageoutput_with_message_text_and_recipent(String arg1, String arg2) throws Throwable {
-		assertThat("Recipent address for send mail is wrong", RECIPENT_ADDRESS.equals(sendMessage.getRecipentMail()));
-		assertThat("Mail message for send mail is wrong", MSG_TEXT.equals(sendMessage.getMsg()));
+	public void i_should_get_a_messageoutput_with_message_text_and_recipent(String message, String recipent)
+			throws Throwable {
+		assertThat("Recipent address for send mail is wrong", recipent.equals(sendMessage.getRecipentMail()));
+		assertThat("Mail message for send mail is wrong", message.equals(sendMessage.getMsg()));
 	}
 }
--- sdc-mail/src/test/resources/cucumber/mail.feature
+++ sdc-mail/src/test/resources/cucumber/mail.feature
@@ -1,11 +1,18 @@
-Feature: MailSend 
-	As a user I want to send a message to given recipent
- 
+Feature: Sending mails and filtering mails tests 
 
+Scenario: As user with id 1 i want to get all output messages that was send by user 3 
+	Given User 3 send an email with text "test1" and recipent "test@t.pl" 
+	When I filter all MessageOutputs by sender with id 3 
+	Then I should have 1 message in filter result 
+	Then I should get a  messageoutput with message text "test1" and recipent "test@t.pl" 
+	
 Scenario: As user with id 1 send mail message to recipent "test@t.pl" with message "test1" 
 	Given I am a user with id 1 
-	When As user 1 I send an email message with "test1" to recipent "test@t.pl" 
+	When I filter all MessageOutputs by sender with id 1
+	Then I should have 0 message in filter result 
+	When As user 1 I send an email message with "test2" to recipent "test2@t.pl" 
 	Then I should recive response with status 200 
 	When I filter all MessageOutputs by sender with id 1 
 	Then I should have 1 message in filter result 
-	And I should get a  messageoutput with message text "test1" and recipent "test@t.pl" 
+	And I should get a  messageoutput with message text "test2" and recipent "test2@t.pl" 
+	
